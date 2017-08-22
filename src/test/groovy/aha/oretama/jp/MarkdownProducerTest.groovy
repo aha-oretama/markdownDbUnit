a7ea1e6dae12b4d9134e5a8ea65cab864293ce3a
aha-oretama <sekine_y_529@msn.com>
2017-08-22 21:28:14 +0900

    Add Spock

---
+++ src/test/groovy/aha/oretama/jp/MarkdownProducerTest.groovy
@@ -0,0 +1,14 @@
+package aha.oretama.jp
+
+import spock.lang.Specification
+
+/**
+ * @author aha-oretama
+ */
+class MarkdownProducerTest extends Specification {
+
+  def produceFromMarkdownTest() {
+    expect:
+    new MarkdownProducer(new File("src/test/resources/testdata.md")).produce()
+  }
+}
---
+++ src/test/resources/testdata.md
@@ -0,0 +1,9 @@
+# User
+| user_id | first_name | last_name | sex |
+|---------| ----------- |:-------- | ----: |
+| 00001 | hogehoge | taro | male |
+
+# Score 
+user_id | subject | score
+------- | :-------: |:-----
+00001 | Mathematics | 100
